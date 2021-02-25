53041498d50c7a10a4ed82767e1b36cad623a34e
Marcestus <55045740+Marcestus@users.noreply.github.com>
2021-02-25 16:55:58 +0200

    Tests: alusta cucumberin käyttö

---
+++ src/test/java/lukuvinkki/RunCucumberTest.java
@@ -0,0 +1,15 @@
+package lukuvinkki;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.junit.CucumberOptions.SnippetType;
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(
+    plugin = "pretty", 
+    features = "src/test/resources/lukuvinkki", 
+    snippets = SnippetType.CAMELCASE 
+)
+
+public class RunCucumberTest {}
---
+++ src/test/java/lukuvinkki/Stepdefs.java
@@ -0,0 +1,12 @@
+package lukuvinkki;
+
+import io.cucumber.java.Before;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+import static org.junit.Assert.*;
+
+public class Stepdefs {
+       
+    
+}
